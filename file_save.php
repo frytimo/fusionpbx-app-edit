bfac27011f5742953dc3ed512281e4b864ff9846
Tim Fry <tim@fusionpbx.com>
2025-03-18 18:41:02 -0300

    Fix too many line feeds when saving files

--- file_save.php
+++ file_save.php
@@ -148,26 +148,14 @@
 						//build a array of the content
 						$lines = explode("\n", str_replace ("\r\n", "\n", $_POST["content"]));
 
-						//go in reverse order to remove blank trailing lines at the end of the file
-						for ($i = count($lines) - 1; $i > 0; $i--) {
-							//find the last empty line
-							if (!empty($lines[$i])) {
-								//exit for loop
-								break;
-							}
-							//remember the last line index
-							$last_line = $i;
-						}
-
+						//remove trailing spaces from each line
 						$file_content = '';
-
-						//remove trailing spaces on each line
-						for ($i = 0; $i < $last_line; $i++) {
-							$file_content .= rtrim($lines[$i]) . "\n";
+						foreach ($lines as $line) {
+							$file_content .= rtrim($line) . "\n";
 						}
 
 						//save the file
-						fwrite($handle, $file_content);
+						fwrite($handle, rtrim($file_content) . "\n");
 
 						//close the file handle
 						fclose($handle);
